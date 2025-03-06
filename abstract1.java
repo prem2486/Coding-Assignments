347307a630c7588163e5a666f54904bd9ac13ce6
PREM KUMAR NEELI <125586273+prem2486@users.noreply.github.com>
2025-03-06 21:11:36 +0530

    Add files via upload

---
+++ abstract1.java
@@ -0,0 +1,8 @@
+package Abstract;
+abstract class AbstractExample {
+    abstract void abstractMethod(); // Abstract method
+
+    void nonAbstractMethod() { // Non-abstract method
+        System.out.println("This is a non-abstract method in an abstract class.");
+    }
+}
