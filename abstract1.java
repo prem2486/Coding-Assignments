96a2653bf39910eda4f9a53c5452bb3b15116ea2
PREM KUMAR NEELI <125586273+prem2486@users.noreply.github.com>
2025-03-06 21:17:44 +0530

    Delete abstract1.java

--- abstract1.java
+++
@@ -1,8 +0,0 @@
-package Abstract;
-abstract class AbstractExample {
-    abstract void abstractMethod(); // Abstract method
-
-    void nonAbstractMethod() { // Non-abstract method
-        System.out.println("This is a non-abstract method in an abstract class.");
-    }
-}
